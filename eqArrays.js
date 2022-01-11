25d7b809c77b16e971d591d1903e1720ba03d87a
Youssef Ragab <ragab.youssef95@gmail.com>
2022-01-11 15:17:35 -0500

    assertEqual added

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false
+      }
+    }
+    return true
+  }
+}
+
+// Tests
+/*
+console.log(eqArrays([1, 2, 3], [1, 2, 3]))
+console.log(eqArrays([1, 2, 3], [3, 2, 1]))
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [1, 2, 31]), true)
+*/

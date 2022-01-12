353e3aa576726d082b07f11817419966317bbb65
Youssef Ragab <ragab.youssef95@gmail.com>
2022-01-11 20:59:49 -0500

    flatten function added

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(a1, a2) {
+  if (eqArrays(a1, a2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${a1} === ${a2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${a1} !== ${a2}`);
+  }
+};
+
+const flatten = function(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        // console.log(arr[i][j])
+        newArray.push(arr[i][j]);
+      }
+    } else {
+      // console.log(arr[i])
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);

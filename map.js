8a2aa9a05c9a56c7664cd02d866d121d84235a88
Youssef Ragab <ragab.youssef95@gmail.com>
2022-01-13 15:01:28 -0500

    add map function

---
+++ map.js
@@ -0,0 +1,32 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(a1, a2) {
+  if (eqArrays(a1, a2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${a1} === ${a2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${a1} !== ${a2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item))
+  }
+  return results
+}
+
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't'])

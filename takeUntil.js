126d00090aec08dfae82fb2539ac1934569136ac
Youssef Ragab <ragab.youssef95@gmail.com>
2022-01-13 15:15:44 -0500

    takeUntil function added

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(a1, a2) {
+  if (eqArrays(a1, a2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${a1} === ${a2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${a1} !== ${a2}`);
+  }
+};
+
+
+
+const takeUntil = function(array, callback) {
+  const result = []
+
+  for (let item of array) {
+    if (callback(item)) {
+      break
+    }
+    result.push(item)
+  }
+  return result
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+assertArraysEqual(takeUntil(data1, x => x <= 0), [1, 2, 5, 7, 2])
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);

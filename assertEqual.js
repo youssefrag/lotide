8dcae99bc50b64ed4892574197d8faa0a0254330
Youssef Ragab <ragab.youssef95@gmail.com>
2022-01-17 13:43:21 -0500

    refactoring/tests

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Youssef", "Youssef");
-assertEqual(10, 1);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Youssef", "Youssef");
+assertEqual(10, 1);
